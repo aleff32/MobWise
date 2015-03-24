094c24b90dbed3b11cbf18e0317361b9268e37e1
Aleff Corrêa Matos <aleff_matos@hotmail.com>
2015-03-24 01:50:22 -0300

    Create Fragment_Video.java

---
+++ Fragment_Video.java
@@ -0,0 +1,19 @@
+package com.example.aleff.desafio;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment_Video extends Fragment {
+
+    View rootView;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
